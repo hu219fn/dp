39790d4c5e66fb61bf4956c00123bea6dc7b1294
hu219fn <142443102+hu219fn@users.noreply.github.com>
2024-07-14 04:12:44 +0300

    edit redme

--- project/urls.py
+++ project/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('app.urls'))
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
